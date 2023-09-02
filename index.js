4f79fdf71790e7dcfaa7e9ec45491ce038a339c6
yurii19 <yurikolechkin@gmail.com>
2023-09-02 09:00:38 +0300

    chore: added expeditions route

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+import { expeditions } from "./src/expeditions";
 
 // Initialize Express
 const app = express();
@@ -8,9 +9,14 @@ app.get("/", (req, res) => {
   res.send("Express on Vercel");
 });
 
+app.get("/expeditions", (req, res) => {
+  res.send(expeditions);
+});
+
 // Initialize server
 app.listen(5000, () => {
   console.log("Running on port 5000.");
 });
 // Export the Express API
 module.exports = app;
+
